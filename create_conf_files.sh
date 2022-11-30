6ab162bc31a6b03f8cdc440e579dd7cde98e00d4
mkenjis <mkenjis@gmail.com>
2022-11-30 14:24:44 -0300

    Refactory using prebuilt java image

--- create_conf_files.sh
+++
@@ -1,9 +0,0 @@
-
-# spark-env.sh (SPARK)
-# ============
-echo 'export JAVA_HOME=/usr/local/jre1.8.0_181' >$SPARK_HOME/conf/spark-env.sh
-chmod +x $SPARK_HOME/conf/spark-env.sh
-
-# log4j.properties (SPARK)
-# ================
-sed '0,/log4j.rootCategory=INFO, console/{s/log4j.rootCategory=INFO, console/log4j.rootCategory=WARN, console/}' $SPARK_HOME/conf/log4j.properties.template >$SPARK_HOME/conf/log4j.properties
